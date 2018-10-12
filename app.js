dc358bec40cc6cc63717e5ea13b1bc17e043a1f3
Shubham Sawant <sawantshubham027@gmail.com>
2018-10-12 12:30:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -54,11 +54,11 @@ var db_pwd  = "cmpe281" ;
 var db_name = "test" ;
 */
 
-var db_host = (process.env.mongodb_host || "localhost" ) ;
-var db_port = (process.env.mongodb_port || "27017" ) ;
-var db_user = (process.env.mongodb_user || "cmpe281" ) ;
+var db_host = (process.env.mongodb_host || "ds131323.mlab.com" ) ;
+var db_port = (process.env.mongodb_port || "31323" ) ;
+var db_user = (process.env.mongodb_user || "admin" ) ;
 var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
-var db_name = (process.env.mongodb_name || "test" ) ;
+var db_name = (process.env.mongodb_name || "test_v1" ) ;
    
 
 var db = new DB(db_name,
